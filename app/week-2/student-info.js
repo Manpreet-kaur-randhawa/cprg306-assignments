41d5b956d099c07161909f25d280c393063e0cb8
Manpreet-kaur-randhawa <Manpreet.Kaur1@edu.sait.ca>
2025-09-16 13:56:02 -0600

    Week 2 finished

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <h1>Manpreet Kaur</h1>
+            <Link href="https://github.com/Manpreet-kaur-randhawa/cprg306-assignments">Link To My Github Repository</Link>
+        </div>
+    )
+}
